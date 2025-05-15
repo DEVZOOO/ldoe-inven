0ec46882de7c3569d642ce945d2e12097116f779
devzooo <devzoou@gmail.com>
2025-05-15 16:45:47 +0900

    feat: font 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 
 import "@/app/styles/reset.scss";
+import "@/app/styles/typography.scss";
+import "@/app/styles/common.scss";
 import "@/app/styles/globals.css";
 
 import Header from "@/components/header";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "LDOE INVEN",
@@ -28,9 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-[480px] mx-auto px-[20px]`}
-      >
+      <body className={`antialiased max-w-[480px] mx-auto px-[20px]`}>
         <Header></Header>
         {children}
       </body>
