662981061a49bdd5b551e6c307da562b53a2b775
devzooo <devzoou@gmail.com>
2025-05-15 16:11:20 +0900

    chore: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className="py-[20px]">
+      <h1>LDOE Inven by devzooo</h1>
+
+      <section>Creatures</section>
+
+      <section>Weapons</section>
+
+      <section>Food</section>
+    </div>
+  );
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header className="flex mt-[12px]">
+      <h1>LOGO</h1>
+      <div className="flex">
+        <input type="text"></input>
+        <button type="button">SEARCH</button>
+      </div>
+    </header>
+  );
+}
