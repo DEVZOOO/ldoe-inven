3a9c9cce0b850677f01d2ebd17dbc0e0d26e5677
devzooo <devzoou@gmail.com>
2025-05-20 18:46:47 +0900

    feat: footer 추가

---
+++ src/components/footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="flex flex-col items-center justify-center py-m font-Pretendard text-s text-white bg-point-bg">
+      <div>지구의 마지막 날: 생존(LDOE) 인벤토리 by devzooo</div>
+      <div>
+        <a href="mailto:devzoou@gmail.com">devzoou@gmail.com</a>
+      </div>
+    </footer>
+  );
+}
