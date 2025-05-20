79203f0940ff1e629f0aef4c6d703614b9f0054e
devzooo <devzoou@gmail.com>
2025-05-20 18:47:01 +0900

    refactor: 메인 section 컴포넌트 수정

--- src/components/main/ctgry-section.tsx
+++ src/components/main/ctgry-section.tsx
@@ -1,13 +1,37 @@
+import styled from "styled-components";
+
 type CtgrySectionType = {
   title?: string;
+  cls?: string;
   children?: React.ReactNode;
 };
 
-export default function CtgrySection({ title, children }: CtgrySectionType) {
+export default function CtgrySection({
+  title,
+  cls,
+  children,
+}: CtgrySectionType) {
   return (
-    <section>
-      {title && <h1>{title}</h1>}
-      <div>{children}</div>
+    <section className={`mt-m border ${cls}`}>
+      <div className="flex justify-between text-[var(--background)] bg-[var(--foreground)]">
+        {title && <Title>{title}</Title>}
+        <MoreBtn type="button">더보기</MoreBtn>
+      </div>
+      <div className="p-xs">{children}</div>
     </section>
   );
 }
+
+const Title = styled.div`
+  padding: var(--gap-xs);
+`;
+
+const MoreBtn = styled.button`
+  margin: var(--gap-xs) 4px;
+  padding: 2px 4px;
+  color: #31cbbe;
+  background-color: var(--c-black);
+  border: 1px solid #31cbbe;
+  border-radius: var(--radius-s);
+  font-size: 0.8em;
+`;
