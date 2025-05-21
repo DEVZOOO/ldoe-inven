abc95fbba2bcfc1bc16e4779773e96ee8e2e99e0
devzooo <devzoou@gmail.com>
2025-05-21 18:26:00 +0900

    feat: 메인에 몹 리스트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,75 @@
 "use client";
 
 import CtgrySection from "@/components/main/ctgry-section";
+import { ApiStatus } from "@/constants/status";
+import { CommonRes } from "@/types/api";
+import { CreaturesInfo } from "@/types/dto";
 import { useEffect, useState } from "react";
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
-  const [creatures, setCreatures] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [creatures, setCreatures] = useState<CreaturesInfo[]>([]);
 
   useEffect(() => {
-    // const fetchCreatures = async () => {
-    //   try {
-    //     const res = await fetch("/api/creatures");
-    //     console.log(res);
-    //     const data = await res.json();
-    //     console.log(data);
-    //     setCreatures(data);
-    //   } catch (err) {
-    //     console.log(err);
-    //   } finally {
-    //     setLoading(false);
-    //   }
-    // };
-    // fetchCreatures();
+    const fetchCreatures = async () => {
+      try {
+        const res = await fetch("/api/creatures?cnt=5");
+        const result: CommonRes<CreaturesInfo[]> = await res.json();
+        if (result.status == ApiStatus.SUCCESS) {
+          const data = result.data;
+          setCreatures(data!);
+        } else {
+          console.error(result);
+          setIsError(true);
+        }
+      } catch (err) {
+        console.log(err);
+        setIsError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCreatures();
   }, []);
 
   return (
     <div className="my-m">
       <CtgrySection title="Creatures">
-        {false ? (
+        {loading ? (
           <div>Loading...</div>
+        ) : isError ? (
+          <div>Error</div>
         ) : (
-          <div>
-            {[1, 2, 3, 4, 5].map((v, i) => (
-              <div key={i}>
-                Creature {i}: {v}
-              </div>
-            ))}
-          </div>
+          <table className="w-full table text-center middle">
+            <colgroup>
+              <col style={{ width: "10%" }}></col>
+              <col></col>
+              <col style={{ width: "20%" }}></col>
+              <col style={{ width: "20%" }}></col>
+            </colgroup>
+            <thead>
+              <tr>
+                <th>NO</th>
+                <th>NAME</th>
+                <th>HEALTH</th>
+                <th>DAMAGE</th>
+              </tr>
+            </thead>
+            <tbody className="text-[0.8em] font-Pretendard">
+              {creatures.map((v, i) => (
+                <tr key={v.cno}>
+                  <td>{creatures.length - i}</td>
+                  <td>
+                    {v.name}
+                    <br></br>({v.nameEn})
+                  </td>
+                  <td>{v.health}</td>
+                  <td>{v.damage}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         )}
       </CtgrySection>
 
