43d18618ce7a6b3c5a218b677da2a68884b5d425
devzooo <devzoou@gmail.com>
2025-05-21 18:24:37 +0900

    refactor: DB 연동 공통 함수 수정

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,14 @@
-import mysql from "mysql2/promise";
+import mariadb from "mariadb";
+
+console.log(`DB_URL: ${process.env.DB_URL}`);
+
+const pool = mariadb.createPool({
+  host: process.env.DB_URL,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: Number(process.env.DB_PORT),
+});
 
 /**
  * DB Connection
@@ -7,8 +17,18 @@ import mysql from "mysql2/promise";
  * @returns result
  */
 export async function exec<T>(sql: string, values?: unknown[]) {
-  const conn = await mysql.createConnection(process.env.DB_URL!); // mysql connect
-  const [rows] = await conn.execute(sql, values);
-  await conn.end();
-  return rows as T[];
+  let conn;
+
+  try {
+    conn = await pool.getConnection();
+    console.log(`[SQL] ${sql}`);
+    const result = await conn.query(sql, values);
+    console.log(`[RESULT] ${result.length}개`);
+    return result as T[];
+  } catch (e) {
+    console.error(`## FAIL:: ${e}`);
+    throw e;
+  } finally {
+    conn?.release();
+  }
 }
