7a98424dc2a65f725e6d7e2d1e421b284b9a20b4
devzooo <devzoou@gmail.com>
2025-06-07 15:42:42 +0900

    chore: eslint 수정
    any 타입 허용

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,7 @@ const eslintConfig = [
     extends: ["next/core-web-vitals", "next/typescript"],
     rules: {
       "@typescript-eslint/no-unused-vars": "off", // 미사용 변수 lint disable
+      '@typescript-eslint/no-explicit-any': 'off',  // any타입 허용
     },
   }),
 ];
