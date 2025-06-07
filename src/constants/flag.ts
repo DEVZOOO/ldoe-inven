5be00e8f1e361c94caa2985e3f55914b007a960c
devzooo <devzoou@gmail.com>
2025-06-07 15:13:56 +0900

    feat: 상수 추가

--- src/constants/flag.ts
+++ src/constants/flag.ts
@@ -1,4 +1,15 @@
+/**
+ * 여부
+ */
 export enum UseYn {
   Y = "Y",
   N = "N",
+}
+
+/**
+ * 정렬 기준
+ */
+export enum SortOrder {
+  ASC = "ASC",
+  DESC = "DESC",
 }
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
 export * from "./status";
 export * from "./api";
 export * from "./flag";
+export * from "./url";
