63e50a8696d0d7dadd1065b79ef4e3c6be2d2582
devzooo <devzoou@gmail.com>
2025-06-24 12:24:34 +0900

    refactor: 몹 리스트 조회 쿼리 동적 매개변수 처리

--- src/app/api/creatures/service.ts
+++ src/app/api/creatures/service.ts
@@ -27,25 +27,50 @@ export async function selectAllCreatures({
              , DATE_FORMAT(CONVERT_TZ(UPD_DT, '${TimeZone.UTC}', '${TimeZone.SEOUL}'), '${DateFormat.YYYY_MM_DD_HH_II_SS}') AS updDt
         FROM CREATURES
     `;
+    const params = [];
 
     // 검색
     if (searchText) {
         // null 컬럼은 coalesce로 처리
-        sql += `WHERE CONCAT_WS(REPLACE(C.NAME, ' ', ''), REPLACE(C.NAME_EN, ' ', ''), COALESCE(C.FOUND, '')) LIKE "%${searchText}%"`;
+        sql += `WHERE CONCAT_WS(REPLACE(C.NAME, ' ', ''), REPLACE(C.NAME_EN, ' ', ''), COALESCE(C.FOUND, '')) LIKE "%?%"`;
+        params.push(searchText);
     }
 
     // 정렬
     if (sortCol) {
-        sql += `ORDER BY ${sortCol} ${sortOrder ?? "ASC"}`;
+        sql += `ORDER BY ? ?`;
+        params.push(sortCol, sortOrder ?? "ASC");
     }
 
     // 카운팅
     if (cnt != 0) {
         const startIdx = (page - 1) * cnt,
             endIdx = startIdx + cnt;
-        sql += `LIMIT ${startIdx}, ${endIdx}`;
+        sql += `LIMIT ?, ?`;
+        params.push(startIdx, endIdx);
     }
 
-    const result = await exec<CreaturesInfo[]>(sql);
+    const result: CreaturesInfo[] = await exec<CreaturesInfo[]>(sql, params);
     return result;
-}
+}   // END
+
+/**
+ * 몹 정보 조회
+ */
+export async function selectCreatureInfo(cno: number) {
+    const sql = `
+        SELECT 
+            CNO AS cno
+            , IMG_URL AS imgUrl
+            , NAME AS name
+            , NAME_EN AS nameEn
+            , HEALTH AS health
+            , DAMAGE AS damage
+            , ARMOUR AS armour
+        FROM CREATURES
+        WHERE CNO = ?
+    `;
+
+    const result: CreaturesInfo[] = await exec<CreaturesInfo[]>(sql, [cno]);
+    return result.length == 0 ? undefined : result[0];
+}   // END
