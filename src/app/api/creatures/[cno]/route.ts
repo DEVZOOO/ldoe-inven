d6e7d407531e6237dab0c589db2ea9f1bf5a8069
devzooo <devzoou@gmail.com>
2025-06-24 12:28:32 +0900

    feat: 관리자 몹 상세정보 조회 추가

---
+++ src/app/api/creatures/[cno]/route.ts
@@ -0,0 +1,38 @@
+import {NextRequest, NextResponse} from "next/server";
+import {selectCreatureInfo} from "@/app/api/creatures/service";
+import {CommonRes, CreaturesInfo} from "@/types";
+import {ApiStatus} from "@/constants";
+
+type getParams = {
+    params: { cno: string }
+};
+
+/**
+ * 몹 상세정보 조회
+ */
+export async function GET(req: NextRequest, ctx: getParams) {
+    const cno = parseInt(ctx.params.cno, 10);
+
+    let result: CommonRes<CreaturesInfo>;
+
+    try {
+        const data = await selectCreatureInfo(cno);
+
+        if (data) {
+            result = {
+                status: ApiStatus.SUCCESS,
+                data,
+            };
+        } else {
+            result = {
+                status: ApiStatus.ERROR,
+                error: `Invalid CNO: ${cno}`,
+            };
+        }
+    } catch (e) {
+        console.error(`## FAIL ::`, e);
+        result = { status: ApiStatus.ERROR, error: e };
+    }
+
+    return NextResponse.json(result);
+}
